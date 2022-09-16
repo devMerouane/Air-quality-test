c7ba93436a9934daac7536a33e8aa9b5cfeecffb
Merouane Rouibah <60047469+devMerouane@users.noreply.github.com>
2022-09-16 22:32:49 +0200

    fix the error middleware

--- src/api/core/middlewares/error.middleware.ts
+++ src/api/core/middlewares/error.middleware.ts
@@ -2,12 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 
 import { Logger } from '../../config/logger.config';
 import { HttpException } from '../exceptions';
-import {HttpStatusEnum} from '../types/enums';
+import { HttpStatusEnum } from '../types/enums';
 
 export const errorMiddleware = (error: HttpException, req: Request, res: Response, _next: NextFunction) => {
   Logger.log('error', error.message);
-  res.status(error.status || HttpStatusEnum.SERVER_ERROR).send({
-    sucess: false,
-    message: error.message,
-  });
+
+  res.status(error.status || HttpStatusEnum.SERVER_ERROR).send(error.message);
 };
